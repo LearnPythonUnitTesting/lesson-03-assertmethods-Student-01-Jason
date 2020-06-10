e889b4d041132392671ff0eab6dae76d92c1f5f8
Student-01-Jason <63417122+Student-01-Jason@users.noreply.github.com>
2020-06-11 00:00:11 +0100

    Update test_person.py

--- test_person.py
+++ test_person.py
@@ -4,4 +4,8 @@ import person
 
 class TestPerson(unittest.TestCase):
     # TODO
-
+    def test_get_full_name(self):
+        p1 = Person('Bill', 'Gates', 15)
+        p2 = Person('Steve', 'Jobs', 25)
+        self.assertEqual(p1.get_full_name(), 'Bill Jobs')
+        self.assertEqual(p2.get_full_name(), 'Steve Gates')
