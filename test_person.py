a1669ab2c9c1f9fd1563379e03d24608c40e024b
Student-01-Jason <63417122+Student-01-Jason@users.noreply.github.com>
2020-06-11 00:06:33 +0100

    Update test_person.py

--- test_person.py
+++ test_person.py
@@ -7,5 +7,5 @@ class TestPerson(unittest.TestCase):
     def test_get_full_name(self):
         p1 = Person('Bill', 'Gates', 15)
         p2 = Person('Steve', 'Jobs', 25)
-        self.assertEqual(p1.get_full_name(), 'Bill Jobs')
-        self.assertEqual(p2.get_full_name(), 'Steve Gates')
+        self.assertEqual(p1.get_full_name(), 'Bill Gates')
+        self.assertEqual(p2.get_full_name(), 'Steve Jobs')
