91f17235ad93aae6ad28f3decffbbe49545d9254
Student-01-Jason <63417122+Student-01-Jason@users.noreply.github.com>
2020-06-11 00:34:22 +0100

    Update test_person.py

--- test_person.py
+++ test_person.py
@@ -9,3 +9,9 @@ class TestPerson(unittest.TestCase):
         p2 = Person('Steve', 'Jobs', 25)
         self.assertEqual(p1.get_full_name(), 'Bill Gates')
         self.assertEqual(p2.get_full_name(), 'Steve Jobs')
+
+    def test_is_adult(self):
+        p1 = Person('Bill', 'Gates', 15)
+        p2 = Person('Steve', 'Jobs', 25)
+        self.assertEqual(p1.is_adult(), False)
+        self.assertEqual(p2.is_adult(), True)
